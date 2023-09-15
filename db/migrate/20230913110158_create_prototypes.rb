3fbda82c725690f56b2428f1e7ef31daa7553a10
tsukitoremon <33226313+tsukitoremon@users.noreply.github.com>
2023-09-15 16:02:46 +0900

    first commit（再修正）

--- db/migrate/20230913110158_create_prototypes.rb
+++ db/migrate/20230913110158_create_prototypes.rb
@@ -1,11 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.string :title
-      t.text :catch_copy
-      t.text :concept
-      t.integer :user_id
-      t.references :user, foreign_key: true
+      t.string :title           ,null: false
+      t.text :catch_copy       ,null: false
+      t.text :concept          ,null: false
+      t.references :user, foreign_key: true       ,null: false
       t.timestamps
     end
   end
--- db/migrate/20230914170316_create_comments.rb
+++ db/migrate/20230914170316_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.text :text
-      t.references :user, foreign_key: true
-      t.references :prototype, foreign_key: true
+      t.text :text       ,null: false
+      t.references :user, foreign_key: true       ,null: false
+      t.references :prototype, foreign_key: true       ,null: false
       t.timestamps
     end
   end
